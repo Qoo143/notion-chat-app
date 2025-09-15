86b1f5ccb926547b1d57da279a1122393f04d6ca
Qoo143 <wayne676803@gmail.com>
2025-09-15 09:04:38 +0800

    移除未用到的config

--- config/index.js
+++ config/index.js
@@ -1,11 +1,7 @@
 // 主配置檔 - 匯出所有配置模組
 module.exports = {
-  app: require('./app'),
   server: require('./server'),
   notion: require('./notion'),
   gemini: require('./gemini'),
-  ui: require('./ui'),
-  intentAnalysis: require('./intentAnalysis'),
-  logging: require('./logging'),
-  performance: require('./performance')
+  intentAnalysis: require('./intentAnalysis')
 };
